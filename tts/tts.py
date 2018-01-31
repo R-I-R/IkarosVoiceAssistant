360c06cf339f92c0b0ec4edd35a1f703e116fcec
Esteban <esteban.naranjo.rayo@gmail.com>
2018-01-31 19:51:09 -0300

    velocidad de reproduccion arreglada

--- tts/tts.py
+++ tts/tts.py
@@ -8,11 +8,6 @@ import sys
 import time
 import os
 
-text_to_speech = TextToSpeechV1(
-    username='464a4abf-53b5-4ff7-a578-1330e295512a',
-    password='UygljpTJFaIb',
-    x_watson_learning_opt_out=True)  # Optional flag
-
 #print(json.dumps(text_to_speech.voices(), indent=2))
 #print(json.dumps(text_to_speech.pronunciation('Watson', pronunciation_format='spr'), indent=2))
 
@@ -21,13 +16,18 @@ def tts(nombre):
 	if isfile(archivo):
 		reproducir(archivo)
 	else:
+
+		text_to_speech = TextToSpeechV1(
+    		username='464a4abf-53b5-4ff7-a578-1330e295512a',
+    		password='UygljpTJFaIb',
+    		x_watson_learning_opt_out=True)  # Optional flag
+
 		with open(join(dirname(__file__), archivo),'wb') as audio_file:
 			audio_file.write(text_to_speech.synthesize(nombre, accept='audio/wav',voice="es-LA_SofiaVoice"))
 			audio_file.close()
 		reproducir(archivo)
 
 def reproducir(file):
-	print("reproduciendo")
 	os.system("aplay "+file)
 
 
