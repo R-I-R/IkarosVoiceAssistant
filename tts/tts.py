5a113f2b39476bed5067e00cd7e8487fcd3415d3
Esteban <esteban.naranjo.rayo@gmail.com>
2018-01-31 19:46:50 -0300

    reproduccion areglada

--- tts/tts.py
+++ tts/tts.py
@@ -6,6 +6,7 @@ import pyaudio
 import wave
 import sys
 import time
+import os
 
 text_to_speech = TextToSpeechV1(
     username='464a4abf-53b5-4ff7-a578-1330e295512a',
@@ -18,54 +19,17 @@ text_to_speech = TextToSpeechV1(
 def tts(nombre):
 	archivo = "audios/"+nombre.replace(" ","")+".wav"
 	if isfile(archivo):
-		#reproducir(archivo)
-		play_audio_file(join(archivo))
+		reproducir(archivo)
 	else:
 		with open(join(dirname(__file__), archivo),'wb') as audio_file:
 			audio_file.write(text_to_speech.synthesize(nombre, accept='audio/wav',voice="es-LA_SofiaVoice"))
 			audio_file.close()
-		#reproducir(archivo)
-		play_audio_file(join(archivo))
+		reproducir(archivo)
 
 def reproducir(file):
 	print("reproduciendo")
+	os.system("aplay "+file)
 
-	ding_wav = wave.open(file, 'rb')
-	ding_data = ding_wav.readframes(1024)
-	audio = pyaudio.PyAudio()
-	stream_out = audio.open(
-		format=audio.get_format_from_width(ding_wav.getsampwidth()),
-		channels=ding_wav.getnchannels(),
-		rate=ding_wav.getframerate(), input=False, output=True)
-	stream_out.start_stream()
-	stream_out.write(ding_data)
-	time.sleep(0.2)
-	stream_out.stop_stream()
-	stream_out.close()
-	audio.terminate()
-
-
-
-def play_audio_file(fname):
-    """Simple callback function to play a wave file. By default it plays
-    a Ding sound.
-
-    :param str fname: wave file name
-    :return: None
-    """
-    ding_wav = wave.open(fname, 'rb')
-    ding_data = ding_wav.readframes(ding_wav.getnframes())
-    audio = pyaudio.PyAudio()
-    stream_out = audio.open(
-        format=audio.get_format_from_width(ding_wav.getsampwidth()),
-        channels=ding_wav.getnchannels(),
-        rate=ding_wav.getframerate(), input=False, output=True)
-    stream_out.start_stream()
-    stream_out.write(ding_data)
-    time.sleep(0.2)
-    stream_out.stop_stream()
-    stream_out.close()
-    audio.terminate()
 
 tts("Hola Mundo")
 
