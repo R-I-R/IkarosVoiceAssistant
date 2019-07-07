48638274d1e5a24daccacabdaa7810ce29fff748
Esteban <esteban.naranjo.rayo@gmail.com>
2019-07-07 16:17:25 -0400

    gurdado de audios

--- snowboy/snowboydecoderIkaros.py
+++ snowboy/snowboydecoderIkaros.py
@@ -198,7 +198,7 @@ class HotwordDetector(object):
         self.audio.terminate()
 
     def guardarAudio(self):
-        archivo = f"./detecciones/{time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime(time.time()))}.wav"
+        archivo = "./detecciones/{}.wav".format(time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime(time.time())))
         waveFile = wave.open(archivo,"wb")
         waveFile.setnchannels(self.detector.NumChannels())
         waveFile.setsampwidth(int(self.detector.BitsPerSample() / 8))
